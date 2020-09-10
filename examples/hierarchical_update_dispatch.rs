238e8e9f8347c67334ab226d93c5c89059619bd1
Temirkhan Myrzamadi <hirrolot@gmail.com>
2020-09-10 17:31:54 +0600

    Complete the main example

--- examples/hierarchical_update_dispatch.rs
+++ examples/hierarchical_update_dispatch.rs
@@ -4,19 +4,23 @@ use derive_more::From;
 use futures::{future, Stream, StreamExt};
 use tokio::stream;
 
+// Imagine you're writing an admin panel. Here are the hierarchy of updates.
 #[derive(From)]
 enum AdminUpdate {
-    RegisterUser(RegisterUserUpdate),
-    DeleteUser(DeleteUserUpdate),
+    // The following two actions are possibly performed by another admin.
+    UserRegistered(UserRegisteredUpdate),
+    UserDeleted(UserDeletedUpdate),
+
+    // An incoming private messages sent to an admin.
     PrivateMessage(PrivateMessageUpdate),
 }
 
-struct RegisterUserUpdate {
+struct UserRegisteredUpdate {
     username: String,
     id: i64,
 }
 
-struct DeleteUserUpdate {
+struct UserDeletedUpdate {
     id: i64,
 }
 
@@ -26,36 +30,48 @@ struct PrivateMessageUpdate {
 
 #[tokio::main]
 async fn main() {
+    // In the real world, you obtain this stream from network.
     let updates = stream::iter(vec![
-        RegisterUserUpdate { username: "Sergey".to_owned(), id: 1414 }.into(),
-        RegisterUserUpdate { username: "Ivan".to_owned(), id: 22 }.into(),
-        DeleteUserUpdate { id: 1414 }.into(),
+        UserRegisteredUpdate { username: "Sergey".to_owned(), id: 1414 }.into(),
+        UserRegisteredUpdate { username: "Ivan".to_owned(), id: 22 }.into(),
+        UserDeletedUpdate { id: 1414 }.into(),
         PrivateMessageUpdate { message: "Dazy has done her project.".to_owned() }.into(),
         // These two spam letters will be filtered:
         PrivateMessageUpdate { message: "Buy our magazine!!!".to_owned() }.into(),
         PrivateMessageUpdate { message: "Learn how to code Python for free!!!".to_owned() }.into(),
     ]);
 
-    let updates = demux!(AdminUpdate { RegisterUser, DeleteUser, PrivateMessage })(panicking())(
+    // Demultiplex updates into three streams:
+    //  1) of UserRegisteredUpdate,
+    //  2) of UserDeletedUpdate,
+    //  3) of PrivateMessageUpdate.
+    //
+    // `panicking()` means that if the demultiplexer fails to redirect an element of
+    // the input stream into one of the output streams, it'll panic.
+    let updates = demux!(AdminUpdate { UserRegistered, UserDeleted, PrivateMessage })(panicking())(
         updates.boxed(),
     );
 
-    dispatch!(updates => register_users, delete_users, private_messages);
+    // Pass the stream:
+    //  1) of UserRegisteredUpdate into process_registered_users
+    //  2) of UserDeletedUpdate into process_deleted_users
+    //  3) of PrivateMessageUpdate into process_private_messages
+    dispatch!(updates => process_registered_users, process_deleted_users, process_private_messages);
 }
 
 // There is exactly one processor for each update kind, reflecting the
 // single-responsibility principle (SRP):
 // https://en.wikipedia.org/wiki/Single-responsibility_principle
 
-async fn register_users<S>(updates: S)
+async fn process_registered_users<S>(updates: S)
 where
-    S: Stream<Item = RegisterUserUpdate>,
+    S: Stream<Item = UserRegisteredUpdate>,
 {
     updates
         .enumerate()
         .for_each_concurrent(None, |(i, update)| async move {
             println!(
-                "Registering user #{} '{}' ({} users registered at all!)...",
+                "User #{} '{}' has been registered ({} users registered at all).",
                 update.id,
                 update.username,
                 i + 1
@@ -64,18 +80,19 @@ where
         .await;
 }
 
-async fn delete_users<S>(updates: S)
+async fn process_deleted_users<S>(updates: S)
 where
-    S: Stream<Item = DeleteUserUpdate>,
+    S: Stream<Item = UserDeletedUpdate>,
 {
     updates
         .for_each_concurrent(None, |update| async move {
-            println!("Deleting user #{}...", update.id);
+            println!("User #{} has been deleted.", update.id);
         })
         .await;
 }
 
-async fn private_messages<S>(updates: S)
+// Displays incoming private messages, filtering spam.
+async fn process_private_messages<S>(updates: S)
 where
     S: Stream<Item = PrivateMessageUpdate>,
 {
